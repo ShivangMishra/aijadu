5b0a90f7e4e985e3b8ab2fcc2594ecb7f62e2513
vivek <coolvivekpathak2004@gmail.com>
2023-09-11 01:36:58 +0530

    changes cloud

--- screen/Promotion.jsx
+++ screen/Promotion.jsx
@@ -73,8 +73,8 @@ const Promotion = () => {
           <Text style={styles.text}>
             EMPLOYABILITY,PROMOTION{`\n`}      & CAREER GROWTH
           </Text>
-          <View style={{ justifyContent:'center'}}>
-          <Image style={{width: "95%" , height: 180,  alignSelf:'center'}} source={require("../assets/AIJadu/Login/infoContainer.png")} />
+          <View style={{ justifyContent:'center', marginTop:20}}>
+          <Image style={{width: "92%" , height: 190,  alignSelf:'center'}} source={require("../assets/AIJadu/Login/infoContainer.png")} />
           </View>
           <Text style={styles.text1}>
             JOBS THAT REQUIRE{`\n`}    SPECIFIC SKILLS
